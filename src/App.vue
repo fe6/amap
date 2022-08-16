fab2629fc180cf40a1bed3d2d01537e093727ca4
天意 <649076408@qq.com>
2022-08-16 16:37:46 +0800

    feat(dev): config dev env

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<!-- @format -->
+
+<script setup lang="ts">
+  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div>amap</div>
+</template>
+
+<style scoped></style>
