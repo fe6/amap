66e68daea469b8e80917f27b631b5d7cc92f4e0d
云圣天 <649076408@qq.com>
2023-11-07 17:34:49 +0800

    config(打包): 排除 @fe6/http

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', '@fe6/water-pro', 'http', 'axios'],
+      external: ['vue', '@fe6/water-pro', '@fe6/http', 'axios'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
